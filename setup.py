a74f8c2c0148d47b16ad00622994498d22169865
Sebastian Thiel <byronimo@gmail.com>
2014-05-19 17:50:33 +0200

    Added distutils setup, let's see if it works as expected [skip ci]

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+pkg_root = 'src/python'
+
+setup(name='bshotgun',
+      version='0.1.0',
+      description='Write better shotgun tools, faster.',
+      author='Sebastian Thiel',
+      author_email='byronimo@gmail.com',
+      url='https://github.com/Byron/bshotgun',
+      packages=find_packages(pkg_root),
+      package_dir={'' : pkg_root},
+     )
